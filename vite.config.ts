e939fe96e0754c8b5f722cae25f4e9406a603c18
Vedant Butle <vedantvbutle@gmail.com>
2026-08-16 16:20:34 +0530

    Configure Nitro for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tsconfigPaths(), tailwindcss()],
+  plugins: [
+    tanstackStart(),
+    nitro(),
+    react(),
+    tsconfigPaths(),
+    tailwindcss(),
+  ],
 });
